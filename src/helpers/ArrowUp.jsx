1295fb935cf419a963c4c460bc84ff8923cb1e55
kzldlgr <kazel_deligero@yahoo.com>
2023-08-01 03:51:34 +0800

    feat/ add scale on hover on arrow up button

--- src/helpers/ArrowUp.jsx
+++ src/helpers/ArrowUp.jsx
@@ -28,7 +28,8 @@ export default function ArrowUp() {
                 <motion.button
                     initial={{ opacity: 0, translateY: 100 }}
                     animate={{ opacity: 1, translateY: 0 }}
-                    transition={{ type: "spring", bounce: 0.25 }}
+                    transition={{ type: "spring", bounce: 0.25, duration: 4 }}
+                    whileHover={{ scale: 1.2 }}
                     onClick={scrollUp}
                     className="rounded-full border w-[30px] h-[30px] border-white/20 flex justify-center items-center group-hover:border-white text-white/20 group-hover:text-white ">
                     <FaArrowUp size={20} />
