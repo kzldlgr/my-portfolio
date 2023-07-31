38060a010fcabffbf69871d59721ec048de99845
kzldlgr <kazel_deligero@yahoo.com>
2023-08-01 02:33:16 +0800

    feat/initial contact page with emailjs function

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,38 @@
-import { motion } from 'framer-motion'
-import { fadeIn } from '../variant'
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variant';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Contact() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_q32cskp', 'template_gqgah4g', form.current, 'j-G073_85dr0O_r_K')
+      .then((result) => {
+        console.log(result.text);
+        toast.success('Email sent! Thank you', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+          });
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
   return (
     <section className="py-16 lg:section" id="contact">
-      <div className="container mx-auto">
+      <div className="container mx-auto mt-4">
         <div className='flex flex-col lg:flex-row'>
           <motion.div
             variants={fadeIn('right', 0.3)}
@@ -13,9 +41,9 @@ export default function Contact() {
             viewport={{ once: false, amount: 0.3 }}
             className='flex-1'>
             <div>
-              <h4 className='text-text uppercase text-xl font-medium mb-2 tracking-wide'>Get in touch</h4>
-              <h2 className='text-[45px] lg:text-[90px] leading-none mb-12'>
-                Let&apos;s work <br /> together!
+              <h4 className='text-text uppercase text-md lg:text-xl font-medium lg:mb-2 tracking-wide'>Get in touch</h4>
+              <h2 className='text-[30px] lg:text-[90px] leading-none mb-6 lg:mb-12'>
+                Let&apos;s work together!
               </h2>
             </div>
           </motion.div>
@@ -24,12 +52,15 @@ export default function Contact() {
             initial="hidden"
             whileInView={'show'}
             viewport={{ once: false, amount: 0.3 }}
-             className='flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start'>
-            <input className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all' type="text" placeholder='Your name' />
-            <input className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all' type="text" placeholder='Your email' />
-            <textarea className='bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12' placeholder='Your message'></textarea>
-            <button className='py-[18px] px-[50px] h-[66px] flex items-center justify-center text-2xl uppercase font-secondary font-semibold bg-primary text-text hover:brightness-75'>Send message</button>
+            ref={form}
+            onSubmit={sendEmail}
+            className='flex-1 border rounded-2xl flex flex-col gap-y-2 lg:gap-y-6 lg:pb-24 pb-12 p-6 items-start'>
+            <input className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all' name="from_name" type="text" placeholder='Your name' />
+            <input className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all' name="from_email" type="text" placeholder='Your email' />
+            <textarea className='bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12' name="message" placeholder='Your message'></textarea>
+            <button className='py-[18px] px-[50px] h-[66px] flex items-center justify-center text-2xl uppercase font-secondary font-semibold bg-primary text-text hover:brightness-75' type='submit' value="Send">Send message</button>
           </motion.form>
+          <ToastContainer />
         </div>
       </div>
     </section>
