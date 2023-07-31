1d5308608f2b91bcb02113fe8daf724db649d74c
kzldlgr <kazel_deligero@yahoo.com>
2023-08-01 03:48:34 +0800

    feat/add hover effect on arrow up button

--- src/helpers/ArrowUp.jsx
+++ src/helpers/ArrowUp.jsx
@@ -23,14 +23,14 @@ export default function ArrowUp() {
     }
 
     return (
-        <nav className="fixed lg:bottom-12 lg:right-12 bottom-4 right-4 z-40">
+        <nav className="fixed lg:bottom-12 lg:right-12 bottom-4 right-4 z-40 group ">
             {backToTop &&
                 <motion.button
                     initial={{ opacity: 0, translateY: 100 }}
                     animate={{ opacity: 1, translateY: 0 }}
                     transition={{ type: "spring", bounce: 0.25 }}
                     onClick={scrollUp}
-                    className="rounded-full border w-[30px] h-[30px] border-white flex justify-center items-center">
+                    className="rounded-full border w-[30px] h-[30px] border-white/20 flex justify-center items-center group-hover:border-white text-white/20 group-hover:text-white ">
                     <FaArrowUp size={20} />
                 </motion.button>
             }
